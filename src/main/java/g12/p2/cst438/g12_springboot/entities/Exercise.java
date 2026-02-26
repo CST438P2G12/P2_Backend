64df4465145823e7f6c18d34c1800bbd12a55503
aleHernandez9725 <alexh1238@gmail.com>
2026-02-25 20:05:04 -0800

    Created the Exercise Entity

---
+++ src/main/java/g12/p2/cst438/g12_springboot/entities/Exercise.java
@@ -0,0 +1,7 @@
+package g12.p2.cst438.g12_springboot.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Exercise {
+}
---
+++ src/main/java/g12/p2/cst438/g12_springboot/entities/Workout.java
@@ -0,0 +1,33 @@
+package g12.p2.cst438.g12_springboot.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Workout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private LocalDate date;
+
+    //@OneToMany(mappedBy = "workout",cascade = CascadeType.ALL)
+
+    @Override
+    public String toString() {
+        return "Workout [id=" + id + ", user=" + user + ", date=" + date + "]";
+    }
+
+
+    protected Workout() {}
+
+    public Workout(User user, LocalDate date) {
+        this.user = user;
+        this.date = date;
+    }
+}
