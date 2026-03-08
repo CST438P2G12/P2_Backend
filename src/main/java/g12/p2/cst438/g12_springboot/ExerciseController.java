f3316eefe1b9817da0586f13db89d47391306a0d
Daniel Everman <daniel@everman.info>
2026-03-07 20:50:49 -0800

    - Finished remaining routes on ExerciseController

--- src/main/java/g12/p2/cst438/g12_springboot/ExerciseController.java
+++ src/main/java/g12/p2/cst438/g12_springboot/ExerciseController.java
@@ -3,10 +3,7 @@ package g12.p2.cst438.g12_springboot;
 import g12.p2.cst438.g12_springboot.entities.Exercise;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -52,5 +49,31 @@ public class ExerciseController {
         return ResponseEntity.status(HttpStatus.CREATED).body(exercise);
     }
 
+    @PutMapping("/updateExercise")
+    public ResponseEntity<Exercise> updateExercise(@RequestBody Exercise exercise) {
+        if (!exerciseRepository.existsById(exercise.getId())) {
+            return ResponseEntity.notFound().build();
+        }
+        exerciseRepository.save(exercise);
+        return ResponseEntity.ok(exercise);
+    }
 
+    @PatchMapping("/patchExercise")
+    public ResponseEntity<Exercise> patchExercise(@RequestBody Exercise exercise) {
+        if (!exerciseRepository.existsById(exercise.getId())) {
+            return ResponseEntity.notFound().build();
+        }
+        exerciseRepository.save(exercise);
+        return ResponseEntity.ok(exercise);
+    }
+
+
+    @DeleteMapping("/deleteExercise")
+    public ResponseEntity<Void> deleteExercise(@RequestBody Exercise exercise) {
+        if (!exerciseRepository.existsById(exercise.getId())) {
+            return ResponseEntity.notFound().build();
+        }
+        exerciseRepository.delete(exercise);
+        return ResponseEntity.noContent().build();
+    }
 }
