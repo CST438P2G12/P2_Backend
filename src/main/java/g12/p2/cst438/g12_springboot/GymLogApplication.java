c5b0bd513113cc5b6682d35b604944c5f87dd18b
Daniel Everman <daniel@everman.info>
2026-03-12 17:32:48 -0700

    - Added CORS mapping for our frontend

--- src/main/java/g12/p2/cst438/g12_springboot/GymLogApplication.java
+++ src/main/java/g12/p2/cst438/g12_springboot/GymLogApplication.java
@@ -2,6 +2,9 @@ package g12.p2.cst438.g12_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GymLogApplication {
@@ -10,4 +13,13 @@ public class GymLogApplication {
         SpringApplication.run(GymLogApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(("http://localhost:3000"));
+            }
+        };
+    }
 }
