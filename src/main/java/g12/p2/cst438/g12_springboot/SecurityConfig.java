525fa89d0d5a2577ebd734d684395c2f5316fc88
Daniel Everman <daniel@everman.info>
2026-03-14 14:52:51 -0700

    - Moved corsConfigurer from GymLogApplication to SecurityConfig - Updated authorization requirements to only require authentication on API endpoints, not other routes

--- src/main/java/g12/p2/cst438/g12_springboot/SecurityConfig.java
+++ src/main/java/g12/p2/cst438/g12_springboot/SecurityConfig.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -16,10 +23,27 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/admin/**", "/patchExercise/**", "/add**", "/delete**", "/get**", "/update**").authenticated()
+                        .anyRequest().permitAll())
                 .oauth2Login(oAuth2Login -> {
                 });
 
         return http.build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(("http://localhost:3000"))
+                        .allowCredentials(true)
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
+    }
+
 }
